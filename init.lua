5d451879b6c30a27268a1a6a4cab7d2912bf55c3
pithydon <daniel@Daniels-iMac.local>
2018-04-30 20:52:22 -0600

    stone stairs

--- init.lua
+++ init.lua
@@ -2,7 +2,39 @@ minetest.override_item("default:stone", {
   tiles = {{name = "default_stone.png", align_style = "world", scale = 2}}
 })
 
+minetest.override_item("stairs:slab_stone", {
+  tiles = {{name = "default_stone.png", align_style = "world", scale = 2}}
+})
+
+minetest.override_item("stairs:stair_stone", {
+  tiles = {{name = "default_stone.png", align_style = "world", scale = 2}}
+})
+
+minetest.override_item("stairs:stair_inner_stone", {
+  tiles = {{name = "default_stone.png", align_style = "world", scale = 2}}
+})
+
+minetest.override_item("stairs:stair_outer_stone", {
+  tiles = {{name = "default_stone.png", align_style = "world", scale = 2}}
+})
+
 minetest.override_item("default:cobble", {
+  tiles = {{name = "default_cobble.png", align_style = "world", scale = 2}}
+})
+
+minetest.override_item("stairs:slab_cobble", {
+  tiles = {{name = "default_cobble.png", align_style = "world", scale = 2}}
+})
+
+minetest.override_item("stairs:stair_cobble", {
+  tiles = {{name = "default_cobble.png", align_style = "world", scale = 2}}
+})
+
+minetest.override_item("stairs:stair_inner_cobble", {
+  tiles = {{name = "default_cobble.png", align_style = "world", scale = 2}}
+})
+
+minetest.override_item("stairs:stair_outer_cobble", {
   tiles = {{name = "default_cobble.png", align_style = "world", scale = 2}}
 })
 
